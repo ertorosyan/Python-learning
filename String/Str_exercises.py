41297739c3a09649969466c14edf024d9caf9ced
Ero <44675987+ertorosyan@users.noreply.github.com>
2019-01-23 18:28:42 +0400

    Update Str_exercises.py

--- String/Str_exercises.py
+++ String/Str_exercises.py
@@ -1,1 +1,16 @@
 
+#2
+                def bar(str):
+                  if len(str) < 2:
+                    return ''
+
+                  return str[:2] + str[-2:]
+
+                print(bar('w3resource'))
+                
+                
+                
+                
+                
+                
+                
